b22a4b5703224655aacd8ec4117a4cbb3d83df54
Brandi L Mason <masonbrandi66@gmail.com>
2023-08-18 11:46:58 -0600

    lab completed. some information missing bc of seed file, let the creator of the content know through github

--- server/models.py
+++ server/models.py
@@ -11,13 +11,56 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+
+    animals = db.relationship('Animal', back_populates='zookeepers')
+
+    def __repr__(self):
+        return f'''
+        <h1>ID: {self.id}</h1>
+        <h1>Name: {self.name}</h1>
+        <h1>Birthday: {self.birthday}</h1>
+        <h1>Animal: {self.animals}</h1>
+        '''
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', back_populates='enclosures')
+
+    def __repr__(self):
+        return f'''
+        <h1>ID: {self.id}</h1>
+        <h1>Environment: {self.environment}</h1>
+        <h1>Open to Visitors: {self.open_to_visitors}</h1>
+        <h1>Animal: {self.animals}</h1>
+        '''
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    zookeepers = db.relationship('Zookeeper', back_populates='animals')
+    enclosures = db.relationship('Enclosure', back_populates='animals')
+
+
+
+    def __repr__(self):
+        return f'''
+        <h1>Name: {self.name}</h1>
+        <h1>Species: {self.species}</h1>
+        <h1>Zookeeper: {self.zookeepers}</h1>
+        <h1>Enclosure: {self.enclosures}</h1>
+        '''
